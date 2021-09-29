f1da2bce3369a2e7b6f5019bfcf430ccb9b5f9f2
jatinderbhola <jatinderbhola@gmail.com>
2021-09-28 23:31:11 -0400

    CR-1 - home component added

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>
+        Welcome to my Homepage
+    </h1>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
