50f290ac810051a5e61ea119192dea10f21a4d09
jatinderbhola <jatinderbhola@gmail.com>
2021-09-28 22:33:23 -0400

    CR-1 - testing photo data service to loads photoes :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
+import PhotoDataService from "./services/photo";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Photos App</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      APP_NAME: "Google Photo Ditto!",
+      images: [{ name: "jatinder1" }, { name: "jatinder2" }, { name: "jatinder3" }, { name: "jatinder4" }]
+    };
+
+  }
+
+  componentDidMount() {
+    PhotoDataService.getAll()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>{this.state.APP_NAME} </h1>
+        </header>
+
+        {
+          this.state.images.map(image => (<h1 key={image.name}> {image.name} </h1>))
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
